bffa961455c25353a3f743bb8669a84939b6a5f1
gildorath <ansorunlabib@myborn.id>
2022-12-03 13:58:12 +0700

    apply shadow DOM on custom component

--- src/script/component/club-list.js
+++ src/script/component/club-list.js
@@ -1,27 +1,39 @@
 import './club-item.js'
-
-class CLubList extends HTMLElement {
-    /**
-     * @param {any} clubs
-     */
+class ClubList extends HTMLElement {
+    constructor() {
+      super();
+      this._shadowHost = this.attachShadow({mode: 'open'});
+    }
+   
     set clubs(clubs) {
-        this._clubs = clubs;
-        this.render();
+      this._clubs = clubs;
+      this.render();
     }
-
-    render() {
-        this.innerHTML = '';
-        this._clubs.forEach(club => {
-            const clubItemElement = document.createElement('club-item');
-            clubItemElement.club = club;
-            this.appendChild(clubItemElement);
-        })
-    }
-
+   
     renderError(message) {
-        this.innerHTML = '';
-        this.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+      this._shadowHost.innerHTML = `
+      <style>
+      .placeholder {
+        font-weight: lighter;
+        color: rgba(0,0,0,0.5);
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+        </style>
+      `;
+      this._shadowHost.innerHTML += `<h2 class="placeholder">${message}</h2>`;
     }
-};
-
-customElements.define('club-list', CLubList)
+   
+    render() {
+      this._shadowHost.innerHTML = '';
+      this._clubs.forEach(club => {
+        const clubItemElement = document.createElement('club-item');
+        clubItemElement.club = club;
+        this._shadowHost.appendChild(clubItemElement);
+      });
+    }
+  }
+   
+  customElements.define('club-list', ClubList);

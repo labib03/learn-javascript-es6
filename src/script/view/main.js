b4a0f597893725a6fe43f5a12ad7a54fc706d300
gildorath <ansorunlabib@myborn.id>
2022-12-03 13:32:05 +0700

    nested custom element

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,9 +1,10 @@
-import '../component/search-bar.js'
+import '../component/search-bar.js';
+import '../component/club-list.js';
 import DataSource from "../data/data-source.js";
 
 const main = () => {
   const searchElement = document.querySelector('search-bar');
-  const clubListElement = document.querySelector('#clubList');
+  const clubListElement = document.querySelector('club-list');
 
   // == asynchronous template ==
   // const onButtonSearchClicked = () => {
@@ -22,29 +23,37 @@ const main = () => {
     }    
   }
 
-  const renderResult = (results) => {
-    clubListElement.innerHTML = '';
-    results.forEach((club) => {
-      const { name, fanArt, description } = club;
+  // const renderResult = (results) => {
+  //   clubListElement.innerHTML = '';
+  //   results.forEach((club) => {
+  //     const { name, fanArt, description } = club;
 
-      const clubElement = document.createElement('div');
-      clubElement.setAttribute('class', 'club');
+  //     const clubElement = document.createElement('div');
+  //     clubElement.setAttribute('class', 'club');
 
-      clubElement.innerHTML = `
-      <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
-      <div class="club-info">
-      <h2>${name}</h2>
-      <p>${description}</p>
-      </div>
-      `
-      clubListElement.appendChild(clubElement);
-    });
-  };
+  //     clubElement.innerHTML = `
+  //     <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
+  //     <div class="club-info">
+  //     <h2>${name}</h2>
+  //     <p>${description}</p>
+  //     </div>
+  //     `
+  //     clubListElement.appendChild(clubElement);
+  //   });
+  // };
+ 
+  // const fallbackResult = (message) => {
+    //   clubListElement.innerHTML = '';
+    //   clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+    // };
+    
+    const renderResult = results => {
+      clubListElement.clubs = results;
+    };
 
-  const fallbackResult = (message) => {
-    clubListElement.innerHTML = '';
-    clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
-  };
+    const fallbackResult = message => {
+      clubListElement.renderError(message);
+    };
 
   searchElement.clickEvent = onButtonSearchClicked;
 };

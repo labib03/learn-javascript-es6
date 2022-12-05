8cc2752c2db3f78018e2e0da1c7f7ed8faa08653
gildorath <ansorunlabib@myborn.id>
2022-12-05 11:18:31 +0700

    implement webpack

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import './styles/style.css';
+import './script/component/app-bar.js';
+import './script/component/search-bar.js';
+import main from "./script/view/main.js";
+
+document.addEventListener('DOMContentLoaded', main);

5b69db4d1873611f49f96b46b5fd779622d7235e
l_asrdn <ansorudinlabib1@gmail.com>
2022-12-17 16:29:12 +0700

    fix lagging issue

--- src/script/searchElement.js
+++ src/script/searchElement.js
@@ -9,6 +9,59 @@ const searchMoviesContainer = $('#search-movies-container')
 
 let isSearching = false
 
+const createCardResultElement = (movies, genres) => {
+    // kerangka card untuk hasil pencarian
+    const cardResultElement = movies.map((item, index) => {
+        const date = new Date(item?.release_date)
+        const month = convertMonth(date.getUTCMonth())
+        const year = date.getFullYear()
+        const day = date.getDate()
+
+        const stringGenres = convertGenre(genres, item?.genre_ids)
+
+        const container = $(
+            `<div class="w-full py-3 px-5 h-[20rem] bg-gradient-to-r from-pink-50 rounded-xl flex gap-1 cursor-pointer transition duration-300 hover:scale-105">`
+        )
+        const image = $(`<div class="overflow-hidden h-full w-1/5">
+    <img class="w-max object-contain transition duration-700 bg-center rounded-xl h-full" src="${RESULT_IMAGE_URL}/${item?.poster_path}" alt="${item?.title}"/>
+    </div>`)
+
+        // description container
+        const descriptionContainer = $(
+            `<div class="p-3 flex flex-col h-full w-4/5 rounded-xl overflow-hidden">
+            <h1 class="text-3xl font-bold tracking-wide">${item?.title}</h1>
+
+            <div class="flex flex-col gap-1">
+            <p class="text-sm">${year}</p>
+            <p class="text-xs">${stringGenres.join(' / ')}</p>
+            <p class="text-xs">Adult film : ${item?.adult ? 'Yes' : 'No'}</p>
+            </div>
+
+           <div class="mt-5 w-auto h-full">
+            <p class="text-base">Overview :</p>
+            <p class="text-base line-clamp-4 text-justify leading-relaxed tracking-wide">${
+                item?.overview
+            }</p>
+           </div>
+
+        </div>`
+        )
+
+        // container append
+        container.append(image, descriptionContainer)
+
+        // const timer = setTimeout(() => {
+        //     container.empty()
+        //     container.append(image, descriptionContainer)
+        //     return clearTimeout(timer)
+        // }, 1500)
+
+        return container
+    })
+
+    return cardResultElement
+}
+
 const createResultMoviesComponent = (movies, genres) => {
     if (movies.length > 1) {
         const titleContainer = $('<div>').addClass('mt-16')
@@ -16,59 +69,10 @@ const createResultMoviesComponent = (movies, genres) => {
             .addClass('text-xl font-semibold tracking-wide')
             .text(`Hasil pencarian untuk : ${searchInput.val()}`)
 
-        // kerangka card untuk hasil pencarian
-        const cardResultElement = movies.map((item, index) => {
-            const date = new Date(item?.release_date)
-            const month = convertMonth(date.getUTCMonth())
-            const year = date.getFullYear()
-            const day = date.getDate()
-
-            const stringGenres = convertGenre(genres, item?.genre_ids)
-
-            const container = $(
-                `<div class="w-full group py-3 px-5 h-[20rem] backdrop-blur-md bg-gradient-to-r from-pink-50 rounded-xl flex gap-1 cursor-pointer transition duration-300 hover:scale-105">`
-            )
-            const image = $(`<div class="overflow-hidden h-full w-1/5">
-        <img class="w-max object-contain transition duration-700 bg-center rounded-xl h-full" src="${RESULT_IMAGE_URL}/${item?.poster_path}" alt="${item?.title}"/>
-        </div>`)
-
-            // description container
-            const descriptionContainer = $(
-                `<div class="p-3 flex flex-col h-full w-4/5 rounded-xl overflow-hidden">
-                <h1 class="text-3xl font-bold tracking-wide">${item?.title}</h1>
-
-                <div class="flex flex-col gap-1">
-                <p class="text-sm">${year}</p>
-                <p class="text-xs">${stringGenres.join(' / ')}</p>
-                <p class="text-xs">Adult film : ${
-                    item?.adult ? 'Yes' : 'No'
-                }</p>
-                </div>
-
-               <div class="mt-5 w-auto h-full">
-                <p class="text-base">Overview :</p>
-                <p class="text-base line-clamp-4 text-justify leading-relaxed tracking-wide">${
-                    item?.overview
-                }</p>
-               </div>
-
-            </div>`
-            )
-
-            // container append
-            container.append(image, descriptionContainer)
-
-            // const timer = setTimeout(() => {
-            //     container.empty()
-            //     container.append(image, descriptionContainer)
-            //     return clearTimeout(timer)
-            // }, 1500)
-
-            return container
-        })
+        const cardResult = createCardResultElement(movies, genres)
 
         titleContainer.append(title)
-        searchMoviesContainer.append(titleContainer, cardResultElement)
+        searchMoviesContainer.append(titleContainer, cardResult)
     } else {
         const titleContainer = $('<div>').addClass('mt-16')
         const title = $('<h1>')
